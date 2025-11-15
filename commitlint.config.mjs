a8780c222a1e9fc6cefa20cb40d1eb346f58385c
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-15 17:34:36 +0000

    feat: add commitlint config

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 1000],
+    'header-max-length': [2, 'always', 120],
+  },
+};
+
+export default config;
