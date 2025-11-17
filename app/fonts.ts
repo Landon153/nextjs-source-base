8d4d4ba3b75a248b88ff72219d688ce9fde6ac2f
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-17 11:05:52 +0000

    feat: add fonts

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Noto_Sans_KR as NotoSansKR } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin', 'vietnamese'],
+  variable: '--font-sans',
+  weight: 'variable',
+});
+
+export const notoSansKR = NotoSansKR({
+  subsets: ['latin', 'vietnamese'],
+  variable: '--font-sans',
+  weight: 'variable',
+});
