d40bf6c83e5d0fb02fad7cb1a164dc51e3bee235
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-25 10:26:33 +0000

    feat: add Filters component for event filtering functionality

---
+++ app/[locale]/(dashboard)/contents/events/_components/filters.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { type HTMLAttributes, type JSX } from 'react';
+
+import { FilterSearch } from '@/app/[locale]/(dashboard)/_components/filter/filter-search';
+import { FilterSelect } from '@/app/[locale]/(dashboard)/_components/filter/filter-select';
+import { ResetFiltersButton } from '@/app/[locale]/(dashboard)/_components/reset-filters-button';
+import { statusOptions } from '@/app/[locale]/(dashboard)/contents/events/_lib/constants';
+import type { GetEventsValues } from '@/app/[locale]/(dashboard)/contents/events/_lib/schema/get-events-schema';
+import { QueryParamKey } from '@/lib/constants';
+import { useUrlParams } from '@/lib/hooks/use-url-params';
+import { cn } from '@/lib/utils';
+
+const resetStateParams: Partial<Record<QueryParamKey, undefined>> = {
+  [QueryParamKey.Search]: undefined,
+  [QueryParamKey.Status]: undefined,
+};
+const resetStateKeys = Object.keys(resetStateParams);
+
+/* -----------------------------------------------------------------------------
+ * Component: Filters
+ * -------------------------------------------------------------------------- */
+
+interface FiltersProps extends HTMLAttributes<HTMLDivElement> {
+  validatedParams: GetEventsValues;
+}
+
+export function Filters({ className, validatedParams, ...props }: FiltersProps): JSX.Element {
+  const urlParams = useUrlParams();
+
+  const handleFilter = (filter: Record<string, string | undefined>): void => {
+    urlParams.push({ ...filter, [QueryParamKey.Page]: undefined });
+  };
+
+  return (
+    <div className={cn('space-y-4', className)} {...props}>
+      <div className="flex flex-wrap gap-4">
+        <FilterSelect
+          options={statusOptions}
+          placeholder="진행 상태"
+          value={validatedParams[QueryParamKey.Status]}
+          onValueChange={(value) => {
+            handleFilter({ [QueryParamKey.Status]: value });
+          }}
+        />
+
+        <FilterSearch
+          placeholder="제목을 입력해주세요"
+          value={validatedParams[QueryParamKey.Search]}
+          onValueChange={(value) => {
+            handleFilter({ [QueryParamKey.Search]: value });
+          }}
+        />
+
+        <ResetFiltersButton
+          resetKeys={resetStateKeys}
+          onClick={() => {
+            urlParams.push({ ...resetStateParams, [QueryParamKey.Page]: undefined });
+          }}
+        />
+      </div>
+    </div>
+  );
+}
