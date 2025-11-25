77da3ea3bb6f6cb00a4021718d888a03454a8e52
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-25 10:15:19 +0000

    feat: add DeleteRowsDialog component for confirming event deletions

---
+++ app/[locale]/(dashboard)/contents/events/_components/delete-rows-dialog.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import type * as ReactTable from '@tanstack/react-table';
+import { type JSX } from 'react';
+
+import { ConfirmDialog } from '@/components/shared/confirm-dialog';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/sonner';
+import { QueryKeys } from '@/lib/constants';
+import { getQueryClient } from '@/lib/get-query-client';
+import { deleteEvents } from '@/lib/services/default/features/events';
+
+interface DeleteRowsDialogProps<TData> {
+  table: ReactTable.Table<TData>;
+}
+
+export function DeleteRowsDialog<TData>({ table }: DeleteRowsDialogProps<TData>): JSX.Element {
+  const queryClient = getQueryClient();
+  const rowIds = Object.keys(table.getState().rowSelection);
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: deleteEvents,
+    onSuccess: async (response) => {
+      if (!response.status) {
+        toast.error('처리 실패했습니다.');
+
+        return;
+      }
+
+      toast.success('처리 완료되었습니다.');
+
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Events] });
+      table.resetRowSelection();
+    },
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(error.message || '처리 실패했습니다.');
+      }
+    },
+  });
+
+  return (
+    <ConfirmDialog
+      loading={isPending}
+      message={{
+        description: '선택한 이벤트를 삭제하시겠습니까?',
+        title: '삭제 안내',
+      }}
+      trigger={
+        <Button className="min-w-28 bg-white" disabled={!rowIds.length} size="xs" variant="outline-2">
+          삭제
+        </Button>
+      }
+      onConfirm={() => mutateAsync({ ids: rowIds })}
+    />
+  );
+}
