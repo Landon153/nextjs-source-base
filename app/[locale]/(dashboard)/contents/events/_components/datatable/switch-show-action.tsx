ebb82a717bcfe37b3af1ad9db25e09a84f712409
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-26 18:56:35 +0000

    feat: add SwitchShowAction component for toggling event visibility

---
+++ app/[locale]/(dashboard)/contents/events/_components/datatable/switch-show-action.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import { type Row } from '@tanstack/react-table';
+import { type JSX } from 'react';
+
+import { toast } from '@/components/ui/sonner';
+import { Switch } from '@/components/ui/switch';
+import { QueryKeys } from '@/lib/constants';
+import { getQueryClient } from '@/lib/get-query-client';
+import { useRouter } from '@/lib/i18n/routing';
+import { toggleShowEvent } from '@/lib/services/default/features/events';
+import { type Event } from '@/lib/services/default/types/event';
+
+export function SwitchShowAction({ row }: { row: Row<Event> }): JSX.Element {
+  const queryClient = getQueryClient();
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: toggleShowEvent,
+    onSuccess: async (response) => {
+      if (!response.status) {
+        toast.error(response.error.messages.at(0) ?? '처리 실패했습니다.');
+
+        return;
+      }
+
+      toast.success('처리 완료되었습니다.');
+
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Events] });
+      router.refresh();
+    },
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(error.message || '처리 실패했습니다.');
+      }
+    },
+  });
+
+  return (
+    <Switch
+      checked={row.getValue<boolean>('isShow')}
+      disabled={isPending}
+      onCheckedChange={() => {
+        mutate({
+          eventId: row.original.id,
+          payload: { isShow: !row.original.isShow },
+        });
+      }}
+    />
+  );
+}
