714c6ee603bbfcadf4b50d368374339d94af6319
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-28 19:34:56 +0000

    feat: implement Datatable component for displaying customer support data

---
+++ app/[locale]/(dashboard)/operations/customer-support/_components/datatable/datatable.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import type { SortingState } from '@tanstack/react-table';
+import { type HTMLAttributes, type JSX } from 'react';
+
+import { columns } from '@/app/[locale]/(dashboard)/operations/customer-support/_components/datatable/columns';
+import { Pagination } from '@/components/shared/pagination';
+import { TableContent } from '@/components/shared/table-content';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { QueryParamKey } from '@/lib/constants';
+import { useTable } from '@/lib/hooks/use-table';
+import { useTableActions } from '@/lib/hooks/use-table-actions';
+import { useRouter } from '@/lib/i18n/routing';
+import { type PaginatedResponse } from '@/lib/services/default/types/common';
+import { type CustomerSupport } from '@/lib/services/default/types/customer-support';
+import { cn } from '@/lib/utils';
+
+interface DatatableProps extends HTMLAttributes<HTMLDivElement> {
+  data: PaginatedResponse<CustomerSupport>;
+  hasFilter: boolean;
+  sorting: SortingState;
+}
+
+export function Datatable({ className, data, sorting, hasFilter, ...props }: DatatableProps): JSX.Element {
+  const router = useRouter();
+  const { isPending, handleSortingChange, handlePaginationChange, handlePaginationPrefetch } = useTableActions();
+  const { table } = useTable(
+    { columns, data: data.items, rowCount: data.meta.totalItems },
+    {
+      sorting,
+      pagination: {
+        pageIndex: data.meta.currentPage - 1,
+        pageSize: data.meta.itemsPerPage,
+      },
+    },
+    {
+      onSortingChange: (value) => {
+        handleSortingChange(value, QueryParamKey.SortBy);
+      },
+      onPaginationChange: (value) => {
+        handlePaginationChange(value, QueryParamKey.Page);
+      },
+    },
+  );
+
+  return (
+    <Card className={cn('min-w-0', className)} {...props}>
+      <CardHeader className="min-h-15 justify-center py-3">
+        <div className="flex flex-wrap items-center justify-between gap-x-4 gap-y-2">
+          <div className="flex items-center gap-5">
+            <CardTitle className="text-sm font-bold">전체 {data.meta.totalItems}</CardTitle>
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardContent className="border-t p-0">
+        <TableContent
+          hasFilter={hasFilter}
+          loading={isPending}
+          table={table}
+          onRowClick={(row) => {
+            router.push(
+              row.original.isAnswer
+                ? `/operations/customer-support/${row.original.id}`
+                : `/operations/customer-support/${row.original.id}/edit`,
+            );
+          }}
+          onRowHover={(row) => {
+            router.prefetch(
+              row.original.isAnswer
+                ? `/operations/customer-support/${row.original.id}`
+                : `/operations/customer-support/${row.original.id}/edit`,
+            );
+          }}
+        />
+      </CardContent>
+
+      <CardFooter className="justify-end p-0">
+        <Pagination
+          className="py-5.5 overflow-auto p-6"
+          currentPage={data.meta.currentPage}
+          totalPages={data.meta.totalPages}
+          totalResults={data.meta.totalItems}
+          onPageChange={(page) => {
+            table.setPageIndex(page - 1);
+          }}
+          onPageHover={(page) => {
+            handlePaginationPrefetch({ pageIndex: page - 1 }, QueryParamKey.Page);
+          }}
+        />
+      </CardFooter>
+    </Card>
+  );
+}
