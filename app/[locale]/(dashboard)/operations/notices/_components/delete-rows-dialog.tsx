9a30cd9f95f286a42c5567b7d69f234f9d81a08f
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-30 19:49:16 +0000

    feat: add DeleteRowsDialog component for confirming deletion of selected notices

---
+++ app/[locale]/(dashboard)/operations/notices/_components/delete-rows-dialog.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+import type * as ReactTable from '@tanstack/react-table';
+import { type JSX } from 'react';
+
+import { ConfirmDialog } from '@/components/shared/confirm-dialog';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/sonner';
+import { QueryKeys } from '@/lib/constants';
+import { getQueryClient } from '@/lib/get-query-client';
+import { deleteNotices } from '@/lib/services/default/features/notices';
+import type { Notice } from '@/lib/services/default/types/notice';
+
+interface ToggleDeleteActionProps<TData> {
+  table: ReactTable.Table<TData>;
+}
+
+export function DeleteRowsDialog<TData extends Notice>({ table }: ToggleDeleteActionProps<TData>): JSX.Element {
+  const queryClient = getQueryClient();
+  const rowIds = Object.keys(table.getState().rowSelection);
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: deleteNotices,
+    onSuccess: async (response) => {
+      if (!response.status) {
+        toast.error('처리 실패했습니다.');
+
+        return;
+      }
+
+      toast.success('처리 완료되었습니다.');
+
+      await queryClient.invalidateQueries({ queryKey: [QueryKeys.Notices] });
+      table.resetRowSelection();
+    },
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(error.message || '처리 실패했습니다.');
+      }
+    },
+  });
+
+  return (
+    <ConfirmDialog
+      loading={isPending}
+      message={{
+        title: '삭제 안내',
+        description: '선택한 관리자 계정을 삭제하시겠습니까?',
+      }}
+      trigger={
+        <Button className="min-w-28 bg-white" disabled={!rowIds.length} size="xs" variant="outline-2">
+          삭제
+        </Button>
+      }
+      onConfirm={() => mutateAsync({ ids: rowIds })}
+    />
+  );
+}
