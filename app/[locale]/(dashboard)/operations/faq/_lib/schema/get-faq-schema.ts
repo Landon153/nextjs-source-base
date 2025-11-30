6361b5bf960400d1728f0b25f2144737addf2615
Nguyễn Thành Đạt - Landon <43365714+Landon153@users.noreply.github.com>
2025-11-30 19:48:52 +0000

    feat: create getFaqSchema for validating FAQ query parameters

---
+++ app/[locale]/(dashboard)/operations/faq/_lib/schema/get-faq-schema.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+import { DisplayStatusValues } from '@/app/[locale]/(dashboard)/operations/faq/_lib/constants';
+import { QueryParamKey } from '@/lib/constants';
+import {
+  dateQuerySchema,
+  limitQuerySchema,
+  pageQuerySchema,
+  searchQuerySchema,
+  sortByQuerySchema,
+} from '@/lib/schema/common-schema';
+import { transformLastArrayItem } from '@/lib/transform';
+
+export const getFaqSchema = z.object({
+  [QueryParamKey.IsShow]: z
+    .union([
+      z.nativeEnum(DisplayStatusValues),
+      z.nativeEnum(DisplayStatusValues).array().transform(transformLastArrayItem),
+    ])
+    .optional(),
+
+  [QueryParamKey.From]: dateQuerySchema,
+  [QueryParamKey.To]: dateQuerySchema,
+  [QueryParamKey.Search]: searchQuerySchema,
+  [QueryParamKey.Page]: pageQuerySchema,
+  [QueryParamKey.Limit]: limitQuerySchema,
+  [QueryParamKey.SortBy]: sortByQuerySchema,
+});
+
+export type GetFaqValues = z.infer<typeof getFaqSchema>;
